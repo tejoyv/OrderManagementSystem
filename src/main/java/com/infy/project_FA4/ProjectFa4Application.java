035959d3aa00d16981478d597144bbb246275c8b
Tejoy Vachhrajani <tejoyv@gmail.com>
2021-03-08 12:11:24 +0530

    Initial Project Setup

---
+++ src/main/java/com/infy/project_FA4/ProjectFa4Application.java
@@ -0,0 +1,13 @@
+package com.infy.project_FA4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFa4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFa4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/infy/project_FA4/ProjectFa4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.project_FA4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFa4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
