6629fd6066ee1388a1d9f829772f19a541f0191e
Tejoy Vachhrajani <tejoyv@gmail.com>
2021-03-09 15:51:33 +0530

    Visitor Product Functionalities

--- Product/src/main/java/com/project/Product/repository/ProductRepository.java
+++ Product/src/main/java/com/project/Product/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.project.Product.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.project.Product.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer>{
-
+	List<Product> findByCategory(String category);
+	List<Product> findByProductname(String productname);
 }
