8b35e26df6780a1428dcd1439257648b92609c55
Tejoy Vachhrajani <tejoyv@gmail.com>
2021-03-09 18:02:50 +0530

    User wishlist composite pk functionality

--- User/src/main/java/com/project/User/dto/WishlistDTO.java
+++ User/src/main/java/com/project/User/dto/WishlistDTO.java
@@ -1,5 +1,34 @@
 package com.project.User.dto;
 
 public class WishlistDTO {
+	int buyerId;
+	int prodId;
+	
+	//default constructor
+	public WishlistDTO() {
+		super();
+	}
+	
+	//parameterized constructor
+	public WishlistDTO(int buyerId, int prodId) {
+		super();
+		this.buyerId = buyerId;
+		this.prodId = prodId;
+	}
 
+	public int getBuyerId() {
+		return buyerId;
+	}
+
+	public void setBuyerId(int buyerId) {
+		this.buyerId = buyerId;
+	}
+
+	public int getProdId() {
+		return prodId;
+	}
+
+	public void setProdId(int prodId) {
+		this.prodId = prodId;
+	}
 }
--- User/src/main/java/com/project/User/entity/Wishlist.java
+++ User/src/main/java/com/project/User/entity/Wishlist.java
@@ -1,5 +1,46 @@
 package com.project.User.entity;
 
-public class Wishlist {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
 
+@Entity
+@IdClass(BuyerProductId.class)
+@Table(name = "wishlist")
+public class Wishlist {
+	@Id
+	int buyerId;
+	@Id
+	int prodId;
+	
+	//default constructor
+	public Wishlist() {
+		super();
+	}
+	
+	//parameterized constructor
+	public Wishlist(int buyerId, int prodId) {
+		super();
+		this.buyerId = buyerId;
+		this.prodId = prodId;
+	}
+
+	public int getBuyerId() {
+		return buyerId;
+	}
+
+	public void setBuyerId(int buyerId) {
+		this.buyerId = buyerId;
+	}
+
+	public int getProdId() {
+		return prodId;
+	}
+
+	public void setProdId(int prodId) {
+		this.prodId = prodId;
+	}
+	
+	
 }
