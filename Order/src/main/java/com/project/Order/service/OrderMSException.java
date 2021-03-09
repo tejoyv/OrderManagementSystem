713aadbd9ee1b32af1ce37c06e1c5c5881186fe8
bhuvantaparia <bhuvantaparia@gmail.com>
2021-03-09 20:45:08 +0530

    Implemented Service, repo and Controller classes for OrderMS

---
+++ Order/src/main/java/com/project/Order/service/OrderMSException.java
@@ -0,0 +1,12 @@
+package com.project.Order.service;
+
+public class OrderMSException extends Exception {
+
+	public OrderMSException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
