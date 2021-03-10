ea9473535efd0f5d43cb7ec345ea80e31a86d09c
bhuvantaparia <bhuvantaparia@gmail.com>
2021-03-10 17:39:32 +0530

    Updated Orderms

--- Order/src/main/java/com/project/Order/controller/OrderController.java
+++ Order/src/main/java/com/project/Order/controller/OrderController.java
@@ -1,9 +1,33 @@
 package com.project.Order.controller;
 
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import com.project.Order.service.OrderMSException;
+import com.project.Order.service.OrderService;
+import com.project.Order.dto.OrderdetailsDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
 public class OrderController {
 
-	public OrderController() {
-		// TODO Auto-generated constructor stub
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	OrderService orderService;
+	
+	//Fetches all Orders
+	@GetMapping(value = "/api/orders", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<OrderdetailsDTO> getAllOrders() throws OrderMSException{
+		logger.info("Fetching all Orders");
+		return orderService.getAllOrders();
 	}
+	
+	
 
 }
--- Order/src/main/java/com/project/Order/service/OrderServiceImpl.java
+++ Order/src/main/java/com/project/Order/service/OrderServiceImpl.java
@@ -1,9 +1,35 @@
 package com.project.Order.service;
 
-public class OrderServiceImpl {
+import java.util.ArrayList;
+import java.util.List;
 
-	public OrderServiceImpl() {
-		// TODO Auto-generated constructor stub
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.Order.dto.OrderdetailsDTO;
+import com.project.Order.entity.Orderdetails;
+import com.project.Order.repository.OrderRepository;
+
+@Service
+public class OrderServiceImpl implements OrderService{
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	OrderRepository orderRepository;
+
+	public List<OrderdetailsDTO> getAllOrders() throws OrderMSException {
+		List<Orderdetails> orderDetails = orderRepository.findAll();
+		List<OrderdetailsDTO> orderDTOs = new ArrayList<>();
+		
+		for(Orderdetails o : orderDetails) {
+			OrderdetailsDTO orders = OrderdetailsDTO.valueOf(o);
+			orderDTOs.add(orders);
+		}
+		logger.info("All Order details : {}", orderDTOs);
+		return orderDTOs;
+		
 	}
-
 }
