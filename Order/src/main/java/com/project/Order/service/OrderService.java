bf721d3a00244fcedb90453f55a1e7433f7b0a9c
Tejoy Vachhrajani <tejoyv@gmail.com>
2021-03-11 17:59:51 +0530

    Added functionality of searching a particular order by orderid

--- Order/src/main/java/com/project/Order/service/OrderService.java
+++ Order/src/main/java/com/project/Order/service/OrderService.java
@@ -1,9 +1,46 @@
 package com.project.Order.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.project.Order.dto.OrderdetailsDTO;
+import com.project.Order.entity.Orderdetails;
+import com.project.Order.repository.OrderRepository;
 
-public interface OrderService {
-	public List<OrderdetailsDTO> getAllOrders() throws OrderMSException;
+@Service
+public class OrderService{
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	OrderRepository orderRepository;
+
+	public List<OrderdetailsDTO> getAllOrders() throws OrderMSException {
+		List<Orderdetails> orderDetails = orderRepository.findAll();
+		List<OrderdetailsDTO> orderDTOs = new ArrayList<>();
+		
+		for(Orderdetails o : orderDetails) {
+			OrderdetailsDTO orders = OrderdetailsDTO.valueOf(o);
+			orderDTOs.add(orders);
+		}
+		logger.info("All Order details : {}", orderDTOs);
+		return orderDTOs;
+		
+	}
+	
+	public OrderdetailsDTO getParticularOrder(Integer orderid) throws OrderMSException{
+		OrderdetailsDTO orderdetailsDTO = null;
+		Optional<Orderdetails> orderdetails = orderRepository.findById(orderid);
+		if(orderdetails!=null) {
+			Orderdetails orderdetails1 = orderdetails.get();
+			orderdetailsDTO = OrderdetailsDTO.valueOf(orderdetails1);
+		}
+		return orderdetailsDTO;
+	}
 }
